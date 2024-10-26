8b6026d950a873c8fd09e2a61b2ddce22f77d44e
weike-tuole <119661918+weike-tuole@users.noreply.github.com>
2024-10-26 20:09:56 +0800

    Delete DAL directory

--- DAL/Class1.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2a6dab5ff67d89c9cdb310d8a390def758bf6c7064336cb82dfd4ff5414a8932
-size 5024
--- DAL/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:32373389e05d69dad13d3d3b534478695042f377bc58ab18af5cd4961e76e30b
-size 1295
